1886c5c3c6775bc88f39da3edd561824e9b5c417
Per Larsson <per.larsson@gmail.com>
2015-09-11 21:46:12 +0200

    Added karma support.

--- src/io/binary-reader.js
+++ src/io/binary-reader.js
@@ -1,26 +1,37 @@
+function advance(reader: BinaryReader, bytes: number): void {
+  reader.position += bytes;
+  reader.bytesRead += bytes;
+}
+
 export class BinaryReader {
 
   constructor(buffer: ArrayBuffer) {
+    this.bytesRead = 0;
+    this.bufferLength = buffer.byteLength;
     this.buffer = buffer;
     this.view = new DataView(buffer);
     this.position = 0;
   }
 
+  seek(offset: number): void {
+    this.position = offset;
+  }
+
   readUint32(): number {
     let value = this.view.getUint32(this.position, true);
-	  this.position += 4;
+	  advance(this, 4);
 	  return value;
   }
 
   readUint16(): number {
     let value = this.view.getUint16(this.position, true);
-	  this.position += 4;
 	  return value;
+	  advance(this, 2);
   }
 
   readFloat32(): number {
     let value = this.view.getFloat32(this.position, true);
-	  this.position += 4;
+	  advance(this, 4);
 	  return value;
   }
 
@@ -30,7 +41,7 @@ export class BinaryReader {
     for (var i = 0; i < length; ++i) {
       value += String.fromCharCode(array[i]);
     }
-    this.position += length;
+    advance(this, length);
     return value;
   }
 }
---
+++ test/io/binary-reader.spec.js
@@ -0,0 +1,8 @@
+import {BinaryReader} from '../../src/io/binary-reader';
+
+describe('binary-reader', () => {
+  it('should throw when initialized with invalid buffer', () => {
+    let a = true;
+    expect(a).toBe(true);
+  });
+});
